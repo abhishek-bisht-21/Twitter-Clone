713c87c63469bc4b3caa4106769119e5fe14118e
Abhishek <abhishek.bisht54@yahoo.com>
2021-06-29 05:50:52 +0530

    setting up views folder

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const path = require('path');
 
-
-
-app.get('/', (req,res) =>{
-	res.send("Welcome to coding blocks");
+mongoose.connect("mongodb://localhost:27017/twitter-clone", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false,
+  useCreateIndex:true
 })
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log(err));
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 
 
-
-
-app.listen(3000, () =>{
-	console.log("Server running at port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
