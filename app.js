7cbdf587d90f6b1e33aec4d84c82d60d7a37e530
Abhishek <abhishek.bisht54@yahoo.com>
2021-06-29 06:31:49 +0530

    setting up userSchema

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
+const session = require('express-session');
 
-mongoose.connect("mongodb://localhost:27017/twitter-clone", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify:false,
-  useCreateIndex:true
-})
-.then(() => console.log("DB Connected"))
-.catch((err) => console.log(err));
+// For Mongoose
+mongoose
+  .connect("mongodb://localhost:27017/twitter-clone", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
+  // Middlewares For ViewEngine and Public Folder
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname,'/public')));
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'/views'));
-
+// Express_Session
+app.use(session({ 		//Usuage
+  secret: 'weneedabettersecret',
+  resave: false,
+  saveUninitialized: true,
+  
+}));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server running at port 3000");
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+    default: "/images/profilePic.jpeg",
+  },
+});
